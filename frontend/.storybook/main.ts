ae91103a0f972f9218887465abf8ff6bc48de198
loi-go-1220 <loi.go1220@gmail.com>
2025-05-27 17:10:33 -0500

    chore: add Storybook setup to Next.js boilerplate

---
+++ frontend/.storybook/main.ts
@@ -0,0 +1,26 @@
+import type { StorybookConfig } from '@storybook/nextjs';
+
+const config: StorybookConfig = {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    {
+      "name": "@storybook/addon-essentials",
+      "options": {
+        "docs": false
+      }
+    },
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions"
+  ],
+  "framework": {
+    "name": "@storybook/nextjs",
+    "options": {}
+  },
+  "staticDirs": [
+    "..\\public"
+  ]
+};
+export default config;
